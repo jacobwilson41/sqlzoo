d768f24c737b6d788bfe82956d55f64c5087df8b
Jacob Wilson <jacobcharles96@gmail.com>
2020-07-21 11:36:24 -0700

    Update 5-SUM-and-COUNT.sql

--- 5-SUM-and-COUNT.sql
+++ 5-SUM-and-COUNT.sql
@@ -1,3 +1,5 @@
+--World Table
+
 --1. Show the total population of the world.
 
 SELECT SUM(population) FROM world
@@ -35,3 +37,70 @@ GROUP BY continent
 SELECT continent FROM world
 GROUP BY continent
 HAVING SUM(population) > 100000000
+
+-- Nobel Table
+
+--1. Show the total number of prizes awarded.
+
+SELECT COUNT(winner) FROM nobel
+
+--2. List each subject - just once
+
+SELECT DISTINsubject FROM nobel
+
+--3. Show the total number of prizes awarded for Physics.
+
+SELECT COUNT(winner) AS Prizes FROM nobel
+WHERE subject='Physics'
+
+--4. For each subject show the subject and the number of prizes.
+
+SELECT subject, COUNT(winner) FROM nobel
+GROUP BY subject
+
+--5. For each subject show the first year that the prize was awarded.
+
+SELECT subject, MIN(yr) AS 'First Awarded' FROM nobel
+GROUP BY subject
+
+--6. For each subject show the number of prizes awarded in the year 2000.
+
+SELECT subject, COUNT(winner) FROM nobel
+WHERE yr=2000
+GROUP BY subject
+
+--7. Show the number of different winners for each subject.
+
+SELECT subject, COUNT(DISTINCT winner) FROM nobel
+GROUP BY subject
+
+--8. For each subject show how many years have had prizes awarded.
+
+SELECT subject, COUNT(DISTINCT yr) AS Years FROM nobel
+GROUP BY subject
+
+--9. Show the years in which three prizes were given for Physics.
+
+SELECT yr FROM nobel
+WHERE subject = 'Physics'
+GROUP BY yr
+HAVING COUNT(winner) = 3
+
+--10. Show winners who have won more than once.
+
+SELECT winner FROM nobel
+GROUP BY winner
+HAVING COUNT(winner) > 1
+
+--11. Show winners who have won more than one subject.
+
+SELECT winner FROM nobel
+GROUP BY winner
+HAVING COUNT(DISTINCT subject) > 1
+
+--12. Show the year and subject where 3 prizes were given. Show only years 2000 onwards.
+
+SELECT yr, subject FROM nobel
+WHERE yr > 1999
+GROUP BY yr, subject
+HAVING COUNT(winner) = 3
